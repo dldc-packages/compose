454a0a2cae157428eeaf1c869f0f45a3e0c2fbc2
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2023-06-16 01:24:40 +0200

    Remove deno build

--- deno/mod.ts
+++
@@ -1,7 +0,0 @@
-export { compose, composeAdvanced } from './Middleware.ts';
-export type { Middleware, Middlewares, Next } from './Middleware.ts';
-export { Stack } from './Stack.ts';
-export type { StackInternal } from './Stack.ts';
-export { createKey } from './Key.ts';
-export type { Key, KeyProvider, KeyConsumer, KeyProviderFn } from './Key.ts';
-export { MiidError } from './MiidError.ts';
